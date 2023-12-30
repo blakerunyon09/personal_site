97aa3954ef389e9f3aa89d8b4ec53f859c8ced4f
brunyon6 <brunyon6@gatech.edu>
2023-12-29 20:53:00 -0700

    deploy script comments

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,7 @@ npm run build
 # add cname file
 echo "blake-runyon.com" > dist/CNAME
 
+# push subtree to gh-pages branch
 git add dist
-
 git commit -m 'deploy'
-
 git subtree push --prefix dist origin gh-pages
